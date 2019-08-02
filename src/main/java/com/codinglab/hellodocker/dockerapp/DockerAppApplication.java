ca07704be4630b34a0f729634fc69f8a10066f76
刘飞飞 <feifei_liu@crfchina.com>
2019-08-02 15:22:42 +0800

    test

--- src/main/java/com/codinglab/hellodocker/dockerapp/DockerAppApplication.java
+++ src/main/java/com/codinglab/hellodocker/dockerapp/DockerAppApplication.java
@@ -3,7 +3,7 @@ package com.codinglab.hellodocker.dockerapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class DockerAppApplication {
 
     public static void main(String[] args) {
